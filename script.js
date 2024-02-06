8d263459c9af5f04a06592aa641b494c9d05e239
Jeremy Glenn I. Panlasigui <panlasiguijeremyglenn49@gmail.com>
2024-02-07 05:23:20 +0800

    Added functionalities for toggle, resizing grid, and color picker

--- script.js
+++ script.js
@@ -3,40 +3,70 @@
 const container =  document.querySelector(".container");
 const rgbBtn = document.querySelector(".random-btn");
 const blackBtn = document.querySelector(".black-btn");
+const colorPicker = document.querySelector('#color-picker');
 
-let grid = +prompt();
+
+
+function clearGrid(){
+    do{
+        container.removeChild(container.firstChild);
+    }while(container.firstChild);
+}
+
+function changeGrid(){
+    let isGrid = +prompt();
+    let grid= "";
+    if(isGrid >= 10 && isGrid <= 100){
+        grid += isGrid
+        clearGrid();
+        createGrid(grid);
+        console.log(grid);
+    }else {
+        alert("Must be greater than 10 and less than 100")
+    };
+}
 
 
 // make divs and append it dynamically
-for(let i = 0; i <= grid; i++){
+function createGrid(grid){
+    for(let i = 0; i < grid; i++){
 
-    const siblingsDiv = document.createElement('div');
-    siblingsDiv.style.border = "(16/ 100)px solid black";
-    siblingsDiv.setAttribute("class" , `siblings-container${i}`)
-    siblingsDiv.style.display = "flex";
-    siblingsDiv.style.flexDirection = "row";
-    siblingsDiv.style.flex = "1";
-    siblingsDiv.style.borderRadius = `${100 / grid}px`;
+        const siblingsDiv = document.createElement('div');
+        siblingsDiv.style.border = "(16/ 100)px solid black";
+        siblingsDiv.setAttribute("class" , `siblings-container`)
+        siblingsDiv.style.display = "flex";
+        siblingsDiv.style.flexDirection = "row";
+        siblingsDiv.style.flex = "1";
+        siblingsDiv.style.backgroundColor = "black";
+        siblingsDiv.style.borderRadius = `${100 / grid}px`;
+        
+        
+        container.appendChild(siblingsDiv);
     
-    
-    container.appendChild(siblingsDiv);
+        // make inner divs and append it dynamically
+        for(let j = 0 ; j < grid; j++){
+            const childDiv = document.createElement('div');
+            childDiv.style.border = "0.16px solid white";
+            childDiv.setAttribute("class", "grid");
+            childDiv.style.flex = "1";
+            childDiv.style.backgroundColor = "darkcyan";
+            childDiv.style.borderRadius = `${100 / grid}px`;
+            siblingsDiv.appendChild(childDiv);
+        }
+    };
 
-    // make inner divs and append it dynamically
-    for(let j = 0 ; j <= grid; j++){
-        const childDiv = document.createElement('div');
-        const selectSiblings = document.querySelectorAll(`.siblings-container${i}`)
-        childDiv.style.border = "0.16px solid white";
-        childDiv.style.flex = "1";
-        siblingsDiv.appendChild(childDiv);
-        childDiv.style.borderRadius = `${100 / grid}px`;
-    }
-};
+}
 
-// getter of childNode
-const divSelector = document.querySelectorAll('.container div div');
+//Clear the Grid
+const resizeBtn = document.querySelector('.resize-btn');
+resizeBtn.addEventListener('click', changeGrid);
+
 
 // Generate Random Colors
-rgbBtn.addEventListener('click', () =>{
+rgbBtn.addEventListener('click', generateRandomColors);
+
+function generateRandomColors(){
+    const divSelector = document.querySelectorAll('.grid');
     divSelector.forEach((div) => {
         div.addEventListener('mouseover', () =>{
             let num1 = Math.random() * 255;
@@ -51,13 +81,13 @@ rgbBtn.addEventListener('click', () =>{
             div.style.margin = "0px";
         })
     });
-})
-
-
-
+}
 
 // Generate Black Colors
-blackBtn.addEventListener('click', () => {
+blackBtn.addEventListener('click',generateBlackColors);
+
+function generateBlackColors(){
+    const divSelector = document.querySelectorAll('.grid'); 
     divSelector.forEach((div) => {
         div.addEventListener('mouseover', () =>{
             
@@ -69,7 +99,45 @@ blackBtn.addEventListener('click', () => {
             div.style.margin = "0px";
         })
     });
-});
+}
+
+// toggle the border of the grid 
+const toggleBtn = document.querySelector('.toggle-btn');
+let isToggled = "";
+toggleBtn.addEventListener('click', toggleGrid);
+
+function toggleGrid(){
+    const divSelector = document.querySelectorAll('.grid');
+    
+    if(isToggled){
+        divSelector.forEach(div =>{
+            div.style.border = "none";
+        })
+        isToggled = false;    
+    }else {
+        divSelector.forEach(div =>{
+            div.style.border = "1px solid white";
+        })
+        isToggled = true;
+    }
+};
 
 
 
+colorPicker.addEventListener("change", changeBackgroundColor);
+
+
+function changeBackgroundColor(){
+    const bgColor = document.querySelectorAll('.siblings-container');
+
+    bgColor.forEach(div =>{
+        div.style.backgroundColor = `${colorPicker.value}`;
+    })
+}
+
+
+
+
+
+// default Size of grid
+createGrid(10);
